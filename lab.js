4b58e792dabf88c0e10a8bc988e6b8f45b8c5b4b
Peter <peterjbark@gmail.com>
2023-06-04 13:36:42 -0700

    Structured questions

---
+++ lab.js
@@ -0,0 +1,85 @@
+// I. Variables & Datatypes 
+ /* A. Q + A
+        1. How do we assign a value to a variable?
+
+        2. How do we change the value of a variable?
+
+        3. How do we assign an existing variable to a new variable?
+
+        4. Remind me, what are declare, assign, and define?
+
+        5. What is pseudocoding and why should you do it?
+
+        6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+*/
+// B. Strings
+
+// C. Booleans
+
+// D. The Farm
+
+// E. Driver's Ed
+
+
+// II. Loops
+// A. The Basics
+
+// B. Get Even
+
+// C. Give Me Five
+
+// D. Savings Account
+
+
+// III. Arrays & Control flow
+// A. Talk About It
+
+// B. Easy Does it
+
+// C. Accessing Elements
+
+// D. Change Values
+
+// E. Mix It Up
+
+// F. Biggie Smalls
+
+// G. Monkey in the Middle
+
+// H. What's in Your Closet?
+
+
+// IV. Functions
+// A. printGreeting
+
+// B. printCool
+
+// C. calculateCube
+
+// D. isVowel
+
+// E. getTwoLengths
+
+// F. getMultipleLengths
+
+// G. maxOfThree
+
+// H. printLongestWord
+
+
+// V. Objects
+// A. Make a User Object
+
+// B. Update the User
+
+// C. Adding Keys and Values
+
+// D. Shopaholic!
+
+// E. Object within an Object
+
+// F. Loops
+
+// G. Functions Operating on Objects
+
